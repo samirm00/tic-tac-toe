52ec709bbed20fb4d0acd25723cf1c12f9162a0b
samirm00 <sammoham.be@gmail.com>
2024-07-14 08:44:20 +0200

    Inital commit

--- src/components/createCell.js
+++ src/components/createCell.js
@@ -2,18 +2,19 @@ import clickCellHandler from '../handlers/clickCellHandler.js';
 
 /**
  * Creates a cell element, assigns it a class and dataset index, and attaches a click event listener.
- * 
+ *
  * @function createCell
  * @param {number} index - The index to be assigned to the cell's dataset.
  * @returns {HTMLDivElement} The created cell element.
- * 
+ *
 */
-const createCell = (index) => 
+const createCell = (index) =>
 {
     const cell = document.createElement('div');
     cell.classList.add('cell');
     cell.dataset.index = index;
-    cell.addEventListener('click', (e) => {
+    cell.addEventListener('click', (e) =>
+    {
         clickCellHandler(e);
     });
     return cell;
